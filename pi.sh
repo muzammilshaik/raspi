997558e9e5ca68480bf0062b5455a1eebbe500d6
muzammil <35516143+muzammilshaik@users.noreply.github.com>
2021-12-04 19:23:27 +0530

    Update pi.sh

--- pi.sh
+++ pi.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-ifconfig wlan0
-sleep 30
+#ifconfig wlan0
+#sleep 30
 #iwconfig wlan0 essid name key password
-iwconfig wlan0 essid muju key 29143500
-sleep 10
-dhclient wlan0
+#iwconfig wlan0 essid muju key 29143500
+#sleep 10
+#dhclient wlan0
+
+wpa_passphrase muju 29143500 | sudo tee /etc/wpa_supplicant.conf
